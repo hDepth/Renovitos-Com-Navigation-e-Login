d69c711b1350a0a68bcf2013cc96e977e18817ff
hDepth <dephenriquejp@gmail.com>
2023-09-25 23:49:36 -0300

    ainda n sei oq eu to fazendo

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -8,59 +8,59 @@ import app from "../../services/firebase";
 import estilos from "../../css/estilocadastro";
 
 export default function Cadastro({ navigation }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  async function createUser() {
+async function createUser() {
     const auth = getAuth(app);
     await createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
+    .then((userCredential) => {
         console.log("cadastrado com sucesso - ", credentials.user.uid);
-      })
-      .catch((error) => {
+    })
+    .catch((error) => {
         console.log("ENOENT ERR!", error.code);
 
         if (error.code == "auth/email-already-in-use") {
-          console.log("Esse Email esta em uso");
+        console.log("Esse Email esta em uso");
         }
-      });
-  }
+    });
+}
 
-  return (
+    return (
     <View style={estilos.containerPrincipal}>
-      <View>
+        <View>
         <Text style={estilos.title}></Text>
-      </View>
+        </View>
 
-      <View style={estilos.area}>
+    <View style={estilos.area}>
         <Text style={estilos.titleArea}>Cadastro</Text>
-      </View>
-      <View style={estilos.container}>
+        </View>
+        <View style={estilos.container}>
         <TextInput
-          style={estilos.caixasTexto}
-          placeholder="email"
-          placeholderTextColor="#313131"
-          value={email}
-          onChangeText={(value) => setEmail(value)}
+        style={estilos.caixasTexto}
+        placeholder="email"
+        placeholderTextColor="#313131"
+        value={email}
+        onChangeText={(value) => setEmail(value)}
         />
         <TextInput
-          style={estilos.caixaSenha}
-          placeholder="senha"
-          placeholderTextColor="#313131"
-          value={password}
-          onChangeText={(value) => setPassword(value)}
+        style={estilos.caixaSenha}
+        placeholder="senha"
+        placeholderTextColor="#313131"
+        value={password}
+        onChangeText={(value) => setPassword(value)}
         />
 
         <TouchableOpacity style={estilos.Botao} onPress={createUser}>
-          <Text style={estilos.textoBotao}>Cadastrar-se</Text>
+        <Text style={estilos.textoBotao}>Cadastrar-se</Text>
         </TouchableOpacity>
 
         <TouchableOpacity 
         onPress={() => navigation.navigate('Login')}
         style={estilos.Botao}>
-          <Text style={estilos.textoBotao}>Entrar</Text>
+        <Text style={estilos.textoBotao}>Entrar</Text>
         </TouchableOpacity>
-      </View>
     </View>
-  );
+    </View>
+);
 }
